68b5be64a3cc20ba955a06a20c5b8f4c1517a9fe
Dmitry Dzhus <dima@dzhus.org>
2012-06-04 22:18:48 +0400

    Switch to ConfigFile for Runner options

--- src/Runner.hs
+++ src/Runner.hs
@@ -4,21 +4,42 @@ where
 
 import System.Random.MWC
 
+import Data.ConfigFile
+import Control.Monad.Error
+
 import DSMC.Constants
 import DSMC.Domain
 import DSMC.Particles
 
+
+flowSection :: String
+flowSection = "Flow"
+
+
+domainSection :: String
+domainSection = "Domain"
+
+
 main :: IO ()
 main =
     let
-        dt = 0.0001
-        tmax = 0.01
-        domain = Box (-1) 1 (-1) 1 (-1) 1
-        t = 300
-        n = 100000
-        m = 29 * amu
-        flow = (10, 0, 0)
+        origin = (0, 0, 0)
     in do
-      g <- create
-      e <- spawnParticles g domain n t m flow
-      printEnsemble e
+      res <- runErrorT $ do
+               cp <- join $ liftIO $ readfile emptyCP "example.tsk"
+               n <- get cp flowSection "n"
+               t <- get cp flowSection "t"
+               m <- get cp flowSection "m"
+               w <- get cp domainSection "w"
+               l <- get cp domainSection "l"
+               h <- get cp domainSection "h"
+               v <- get cp flowSection "flowVelocity"
+               return $ (Flow n t (m * amu) v, 
+                         makeDomain origin w l h)
+      case res of
+        Left e -> print e
+        Right (flow, domain) -> do
+                 print flow
+                 g <- create
+                 e <- spawnParticles g domain flow
+                 printEnsemble $ fromUnboxed1 e
