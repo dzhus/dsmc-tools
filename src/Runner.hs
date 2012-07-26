731b20d23a3db41da34c34f70af0ed6cfd4daf6b
Dmitry Dzhus <dima@dzhus.org>
2012-07-26 04:41:55 +0400

    Save macroscopic data

--- src/Runner.hs
+++ src/Runner.hs
@@ -4,16 +4,29 @@ module Main
 
 where
 
-import Data.ConfigFile
 import Control.Monad.Error
+
+import Data.ByteString.Char8 as B
+
+import Data.ConfigFile
+
+import Data.Vector.Unboxed as VU
+
+import System.IO
 
 import DSMC
 import DSMC.Domain
+import DSMC.Macroscopic
 import DSMC.Particles
 import DSMC.Traceables
 import DSMC.Util.Constants
+import DSMC.Util.Vector
+
+import Data.Array.Repa as R
 
 import GHC.Conc.Sync
+
+
 
 simSection :: String
 simSection = "Simulation"
@@ -35,6 +48,7 @@ main :: IO ()
 main =
     let
         origin = (0, 0, 0)
+        -- TODO Load body from external definition
         body = (sphere (0, 0, 0) 0.3)
     in do
       split <- getNumCapabilities
@@ -56,20 +70,36 @@ main =
                mx <- get cp macroSection "mx"
                my <- get cp macroSection "my"
                mz <- get cp macroSection "mz"
+               -- TODO Tidy this up
                return $ (Flow n t (m * amu) v sw, ex, dt, ssteps, sepsilon, emptys, mx, my, mz,
                          makeDomain origin w l h)
       case res of
         Left e -> print e
         Right (flow, ex, dt, ssteps, sepsilon, emptys, mx, my, mz, domain) -> do
-                 -- s1 <- create >>= save
-                 -- s2 <- create >>= save
-                 -- s3 <- create >>= save
-                 -- s4 <- create >>= save
-                 -- s5 <- create >>= save
-                 -- s6 <- create >>= save
-                 -- let !(e, _) = openBoundaryInjection (s1, s2, s3, s4, s5, s6) domain ex flow emptyEnsemble
-                 !e <- simulate domain body flow dt emptys ex sepsilon ssteps (mx, my, mz) split
+                 !(e, macro) <- simulate domain body flow dt emptys ex sepsilon ssteps (mx, my, mz) split
 --                 printEnsemble e
+                 print $ R.extent macro
                  print "Finished!"
---                 print $ R.extent e
+                 _ <- saveMacroscopic "macro.txt" macro
                  return ()
+
+fd :: Double -> ByteString
+fd = pack . show
+{-# INLINE fd #-}
+
+ws :: ByteString
+ws = B.singleton ' '
+
+nl :: ByteString
+nl = B.singleton '\n'
+
+
+formatMacroParameter ((x, y, z), (n, (u, v, w), c)) =
+    if n == 0.0 then B.empty
+    else B.concat [fd x, ws, fd y, ws, fd z, ws, fd u, ws, fd v, ws, fd w, nl]
+    
+
+saveMacroscopic path macro = do
+    h <- openFile path WriteMode
+    VU.forM_ (R.toUnboxed macro) $ \p -> do
+      hPut h (formatMacroParameter p)
