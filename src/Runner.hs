d3125ca7ea8196d307ddf0e2181a90cf5a1ec610
Dmitry Dzhus <dima@dzhus.org>
2012-09-06 04:39:44 +0400

    Logging and cleanups in Runner

--- src/Runner.hs
+++ src/Runner.hs
@@ -1,25 +1,34 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main
 
 where
 
 import Control.Monad.Error
+import Control.Exception
 
 import Data.ByteString.Char8 as B
 
 import Data.ConfigFile
+
+import Data.Functor
 
 import Data.Vector.Unboxed as VU hiding ((++))
 
 import System.IO
 import System.Environment
 
+import System.Log
+import System.Log.Logger
+import System.Log.Formatter
+import System.Log.Handler (setFormatter)
+import System.Log.Handler.Simple
+
 import DSMC
 import DSMC.Domain
 import DSMC.Macroscopic
 import DSMC.Particles
-import DSMC.Traceables
 import DSMC.Traceables.Parser
 import DSMC.Util.Constants
 import DSMC.Util.Vector
@@ -27,7 +36,6 @@ import DSMC.Util.Vector
 import Data.Array.Repa as R hiding ((++))
 
 import GHC.Conc.Sync
-
 
 
 simSection :: String
@@ -54,54 +62,82 @@ main =
     let
         origin = (0, 0, 0)
     in do
+      -- | Setup logging to stderr
+      let lf = simpleLogFormatter "$time: [$prio] $msg"
+      lh <- (flip setFormatter) lf <$> verboseStreamHandler stderr DEBUG
+      updateGlobalLogger rootLoggerName (setLevel DEBUG . setHandlers [lh] )
+
+      debugM rootLoggerName "Program started"
+
       [probDef, macroFile, ensFile] <- getArgs
-      split <- getNumCapabilities
-      print $ "Using problem definition file: " ++ probDef
-      res <- runErrorT $ do
-               cp <- join $ liftIO $ readfile emptyCP probDef
-               -- Must refactor this somehow (actually, write CmdArgs
-               -- for .ini files)
-               n <- get cp flowSection "n"
-               t <- get cp flowSection "t"
-               m <- get cp flowSection "m"
-               v <- get cp flowSection "velocity"
-               sw <- get cp flowSection "fn"
-               w <- get cp domainSection "w"
-               l <- get cp domainSection "l"
-               h <- get cp domainSection "h"
-               ex <- get cp simSection "ex"
-               dt <- get cp simSection "dt"
-               ssteps <- get cp simSection "steady_steps"
-               sepsilon <- get cp simSection "steady_epsilon"
-               emptys <- get cp simSection "empty_start"
-               mx <- get cp macroSection "mx"
-               my <- get cp macroSection "my"
-               mz <- get cp macroSection "mz"
-               bodyDef <- get cp bodySection "definition"
-               return $ (Flow n t (m * amu) v sw, ex, dt, 
-                         ssteps, sepsilon, emptys, 
-                         mx, my, mz,
-                         makeDomain origin w l h,
-                         bodyDef)
-      case res of
-        Left e -> print e
-        Right (flow, ex, dt, 
-               ssteps, sepsilon, emptys, 
-               mx, my, mz, 
-               domain, 
-               bodyDef) -> do
-                 -- Try to read body from definition file
-                 body <- parseBodyFile bodyDef
-                 case body of
-                   Left e -> error $ "Problem when reading body definition " ++ bodyDef ++ ": " ++ e
-                   Right b -> do
-                             -- Run the simulation, obtaining iteration count and final distributions
-                             !(iters, e, macro) <- simulate domain b flow dt emptys ex sepsilon ssteps (mx, my, mz) split
-                             print $ R.extent macro
-                             print $ "Finished after " ++ (show iters) ++ " iterations"
-                             _ <- saveMacroscopic macro "macro.txt"
-                             _ <- saveEnsemble e "ensemble.txt"
-                             return ()
+      debugM rootLoggerName $ "Using problem file: " ++ probDef
+      debugM rootLoggerName $ "Macroscopic data output file: " ++ macroFile
+      debugM rootLoggerName $ "Ensemble data output file: " ++ ensFile
+
+      -- Use number of cores as split factor for parallelizing
+      -- stochastic processes.
+      nsplit <- getNumCapabilities
+
+      (hr :: Either IOException Handle) <- try (openFile probDef ReadMode)
+      case hr of
+        Left e -> errorM rootLoggerName $ show e
+        Right hdl -> do
+          res <- runErrorT $ do
+                   cp <- join $ liftIO $ readhandle emptyCP hdl
+                   -- Must refactor this somehow (actually, write CmdArgs
+                   -- for .ini files)
+                   n <- get cp flowSection "n"
+                   t <- get cp flowSection "t"
+                   m <- get cp flowSection "m"
+                   v <- get cp flowSection "velocity"
+                   sw <- get cp flowSection "fn"
+                   w <- get cp domainSection "w"
+                   l <- get cp domainSection "l"
+                   h <- get cp domainSection "h"
+                   ex <- get cp simSection "ex"
+                   dt <- get cp simSection "dt"
+                   ssteps <- get cp simSection "steady_steps"
+                   sepsilon <- get cp simSection "steady_epsilon"
+                   emptys <- get cp simSection "empty_start"
+                   mx <- get cp macroSection "mx"
+                   my <- get cp macroSection "my"
+                   mz <- get cp macroSection "mz"
+                   bodyDef <- get cp bodySection "definition"
+                   return $ (Flow n t (m * amu) v sw, ex, dt,
+                             ssteps, sepsilon, emptys,
+                             mx, my, mz,
+                             makeDomain origin w l h,
+                             bodyDef)
+          case res of
+            Left e -> errorM rootLoggerName $
+                      "Problem when reading problem definition: " ++ show e
+            Right (flow, ex, dt,
+                   ssteps, sepsilon, emptys,
+                   mx, my, mz,
+                   domain,
+                   bodyDef) -> do
+                     -- Try to read body from definition file
+                     debugM rootLoggerName $
+                            "Using body definition file: " ++ bodyDef
+                     body <- parseBodyFile bodyDef
+                     case body of
+                       Left e -> errorM rootLoggerName $
+                                 "Problem when reading body definition: " ++ e
+                       Right b -> do
+                                 debugM rootLoggerName $ "Starting simulation"
+                                 -- Run the simulation, obtaining
+                                 -- iteration count and final
+                                 -- distributions
+                                 !(iters, e, macro) <-
+                                     simulate domain b flow dt
+                                              emptys ex sepsilon ssteps
+                                              (mx, my, mz) nsplit
+                                 print $ R.extent macro
+                                 print $ "Finished after " ++
+                                           (show iters) ++ " iterations"
+                                 _ <- saveMacroscopic macro macroFile
+                                 _ <- saveEnsemble e ensFile
+                                 return ()
 
 
 fd :: Double -> ByteString
@@ -118,7 +154,7 @@ nl = B.singleton '\n'
 
 
 -- | Save Repa array in text file, one element per line.
-saveRepa :: (Unbox e) => 
+saveRepa :: (Unbox e) =>
             (e -> ByteString)
          -- ^ Formatter function.
          -> R.Array U sh e
